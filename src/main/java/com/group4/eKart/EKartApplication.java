d633bc0797dbae40b1c3a27271a62185d2c17566
Deepak Jain <deepak.jain04@sap.com>
2025-04-24 09:49:40 +0530

    initial commit

---
+++ src/main/java/com/group4/eKart/EKartApplication.java
@@ -0,0 +1,13 @@
+package com.group4.eKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKartApplication.class, args);
+	}
+
+}
