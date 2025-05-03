0f1f3b5aca8655832e09b8333057f004eebaae8e
Deepak Jain <deepak.jain04@sap.com>
2025-05-03 10:19:57 +0530

    Added profile validations

--- src/main/java/com/group4/eKart/service/ProfileServiceImpl.java
+++ src/main/java/com/group4/eKart/service/ProfileServiceImpl.java
@@ -1,8 +1,11 @@
 package com.group4.eKart.service;
 
-import com.group4.eKart.dto.SalesSummaryDTO;
 import com.group4.eKart.model.*;
 import com.group4.eKart.repository.ProfileRepository;
+import com.group4.eKart.validator.ProfileValidations;
+import org.hibernate.service.spi.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,67 +15,106 @@ import java.util.UUID;
 
 @Service
 public class ProfileServiceImpl implements ProfileService {
+    private static final Logger logger = LoggerFactory.getLogger(ProfileServiceImpl.class);
+
+    private ProfileValidations profileValidations;
+
+    @Autowired
+    public ProfileServiceImpl(ProfileValidations profileValidations) {
+        this.profileValidations = profileValidations;
+    }
 
     @Autowired
     ProfileRepository profileRepository;
 
     @Override
-    public Profile registerProfile(Profile profile) {
-        return profileRepository.save(profile);
+    public Profile registerProfile(Profile profile) throws ServiceException{
+        logger.debug("Inside registerProfile method");
+        try{
+            logger.info("Validating profile...");
+            profileValidations.validateProfile(profile);
+            return profileRepository.save(profile);
+        } catch (Exception exception) {
+            logger.error("{}\nProfile creation failed!", exception.getMessage());
+            return null;
+        }
     }
 
     @Override
     public Profile getUserById(UUID profileId) {
+        logger.debug("Inside getUserById method");
         return profileRepository.findById(profileId).get();
     }
 
     @Override
     public List<Profile> getAllProfiles() {
+        logger.debug("Inside getAllProfiles method");
         return profileRepository.findAll();
     }
 
     @Override
     public Profile updateProfile(Profile profile) {
-        Profile updatedProfile = profileRepository.findById(profile.getProfileId()).get();
-        updatedProfile.setName(profile.getName());
-        updatedProfile.setPhno(profile.getPhno());
-        updatedProfile.setAddress(profile.getAddress());
-        profileRepository.save(updatedProfile);
-        return updatedProfile;
+        logger.debug("Inside updateProfile method");
+        try{
+            Profile updatedProfile = profileRepository.findById(profile.getProfileId()).get();
+            updatedProfile.setName(profile.getName());
+            profileValidations.validatePhone(profile.getPhno());
+            updatedProfile.setPhno(profile.getPhno());
+            updatedProfile.setAddress(profile.getAddress());
+            profileRepository.save(updatedProfile);
+            return updatedProfile;
+        } catch (Exception exception) {
+            logger.error("{}\nFailed to update profile!", exception.getMessage());
+            return null;
+        }
     }
 
     @Override
     public boolean deleteProfile(UUID profileId) {
+        logger.debug("Inside deleteProfile method");
         if (profileRepository.existsById(profileId)) {
             profileRepository.deleteById(profileId);
+            logger.info("Profile deleted successfully");
             return true;
         }
+        logger.error("Profile not found");
         return false;
     }
 
     @Override
     public Profile findByUsername(String username) {
+        logger.debug("Inside findByUsername method");
         return profileRepository.findByUsername(username);
     }
 
     @Override
     public boolean changePassword(UUID profileId, String oldPassword, String newPassword) {
-        Profile profile = profileRepository.findById(profileId).get();
-        if (Objects.equals(profile.getPassword(), oldPassword)){
-            profile.setPassword(newPassword);
-            profileRepository.save(profile);
-            return true;
+        logger.debug("Inside changePassword method");
+        try{
+            Profile profile = profileRepository.findById(profileId).get();
+            if (Objects.equals(profile.getPassword(), oldPassword)){
+                profileValidations.validatePassword(profile.getPassword());
+                profile.setPassword(newPassword);
+                profileRepository.save(profile);
+                return true;
+            }
+            logger.error("Your old password does nto match");
+            return false;
+        } catch (Exception exception) {
+            logger.info("Error occurred while updating password:{}", exception.getMessage());
+            return false;
         }
-        return false;
     }
 
     @Override
     public boolean deactivateAccount(UUID profileId, String password) {
+        logger.debug("Inside deactivateAccount method");
         Profile profile = profileRepository.findById(profileId).get();
         if (Objects.equals(profile.getPassword(), password)){
             profileRepository.deleteById(profileId);
             return true;
         }
+        logger.error("Error deleting the account");
         return false;
     }
 }
---
+++ src/main/java/com/group4/eKart/validator/ProfileValidations.java
@@ -0,0 +1,36 @@
+package com.group4.eKart.validator;
+
+import com.group4.eKart.model.Profile;
+import com.group4.eKart.model.Roles;
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfileValidations {
+    public void validateUsername(String username) {
+        if (username == null || username.trim().isEmpty()) {
+            throw new ServiceException("Username cannot be null or empty.");
+        }
+    }
+
+    public void validatePassword(String password) {
+        if (password == null || password.length() < 6) {
+            throw new ServiceException("Password must be at least 6 characters long.");
+        }
+        if (!password.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{6,}$")) {
+            throw new ServiceException("Password must contain uppercase, lowercase, and a number.");
+        }
+    }
+
+    public void validatePhone(String phoneNumber) {
+        if (!phoneNumber.isEmpty() && !phoneNumber.matches("^[0-9]{10}$")) {
+            throw new ServiceException("Phone number must be 10 digits.");
+        }
+    }
+
+    public void validateProfile(Profile profile) throws ServiceException{
+        validateUsername(profile.getUsername());
+        validatePhone(profile.getPhno());
+        validatePassword(profile.getPassword());
+    }
+}
