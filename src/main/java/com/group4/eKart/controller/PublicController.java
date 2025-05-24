8dcadfbe625de8f76b8b12ff3ac85c317a097cb2
Deepak Jain <deepak.jain04@sap.com>
2025-05-24 17:27:55 +0530

    Implement public controller

--- src/main/java/com/group4/eKart/controller/PublicController.java
+++ src/main/java/com/group4/eKart/controller/PublicController.java
@@ -1,9 +1,14 @@
 package com.group4.eKart.controller;
 
+import com.group4.eKart.model.Product;
 import com.group4.eKart.model.Profile;
+import com.group4.eKart.service.ProductServiceImpl;
 import com.group4.eKart.service.ProfileServiceImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +19,21 @@ public class PublicController {
     @Autowired
     ProfileServiceImpl profileService;
 
-    @PostMapping("profile/register")
+    @Autowired
+    ProductServiceImpl productService;
+
+    @GetMapping("/healthCheck")
+    public String healthCheck() {
+        return "OK";
+    }
+
+    @PostMapping("/profile/register")
     public Profile registerProfile(@RequestBody Profile newProfile) {
         return profileService.registerProfile(newProfile);
+    }
+
+    @GetMapping("/products/getAll")
+    public List<Product> viewAllProducts() {
+        return productService.viewAllProducts();
     }
 }
